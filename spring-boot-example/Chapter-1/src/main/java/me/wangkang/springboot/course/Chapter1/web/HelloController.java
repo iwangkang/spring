441c7fd8fd91f83aee91a851ffb674fe13daf778
wangkang <admin@wangkang.me>
2017-10-13 11:53:53 +0800

    添加requestMapping示例

--- spring-boot-example/Chapter-1/src/main/java/me/wangkang/springboot/course/Chapter1/web/HelloController.java
+++ spring-boot-example/Chapter-1/src/main/java/me/wangkang/springboot/course/Chapter1/web/HelloController.java
@@ -1,14 +1,31 @@
 package me.wangkang.springboot.course.Chapter1.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
-	@RequestMapping("/hello")
-	public String index(){
+	@RequestMapping(value="/index", method=RequestMethod.GET, consumes="application/json", produces="application/json", params="id=a", headers="Referer=http://wangkang.me")
+	public String index(String id, String str){
 		return "Hello world!";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello world!";
+	}
+	
+	@RequestMapping(value="/test2/{id}", method=RequestMethod.GET)
+	public String test2(@PathVariable String id){
+		return "id is " + id;
+	}
+	
+	@RequestMapping(value="/test3/{[^(A-Za-z)]}")
+	public String test3(@PathVariable String str) {
+		return "str is " + str;
 	}
 	
 }
